e335a0f8a71d29f0b647fe7ed69450ed156373b0
mateuszurban <65718811+mateuszurban@users.noreply.github.com>
2021-02-23 16:25:15 +0100

    Added local storage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uniqid from 'uniqid';
 import './App.css';
 import List from './components/List';
 import Form from './components/Form';
 
 function App() {
+  // State-related:
   // Creates array of objects, to be displayed as a list. For now, it contains dummy objects, used in the List.js component.
   const [plants, setPlants] = useState([
     { name: 'tomato', days: 151, key: uniqid() },
     { name: 'pepper', days: 210, key: uniqid() },
     { name: 'spinach', days: 55, key: uniqid() },
   ]);
+
   const [formVisibility, setFormVisibility] = useState(false);
   const [inputName, setInputName] = useState('');
   const [inputDays, setInputDays] = useState('');
+
+  //Local storage logic:
+  // Load storage on start:
+  useEffect(() => {
+    getLocalStorage();
+  }, []);
+
+  // Save storage on [plants] change
+  useEffect(() => {
+    setLocalStorage();
+  }, [plants]);
+
+  // Load 'plants from local storage. If localStorage is empty - starts w/ empty array.
+  const getLocalStorage = () => {
+    if (localStorage.getItem('plants') === null) {
+      localStorage.setItem('plants', JSON.stringify([]));
+    } else {
+      let storedData = JSON.parse(localStorage.getItem('plants'));
+      setPlants(storedData);
+    }
+  };
+
+  // Save 'plants' to local storage.
+  const setLocalStorage = () => {
+    localStorage.setItem('plants', JSON.stringify(plants));
+  };
 
   // Toggles form visibility. Sets boolean 'formVisibility' value to the opposite.
   const toggleFormDisplay = () => {
