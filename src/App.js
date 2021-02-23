a09f420ab90eb433cc1ea1246e72517ec1f471e0
mateuszurban <65718811+mateuszurban@users.noreply.github.com>
2021-02-23 13:29:33 +0100

    Created form to add new plants. localStorage not set up yet.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import uniqid from 'uniqid';
 import './App.css';
 import List from './components/List';
+import Form from './components/Form';
 
 function App() {
   // Creates array of objects, to be displayed as a list. For now, it contains dummy objects, used in the List.js component.
@@ -10,10 +11,34 @@ function App() {
     { name: 'pepper', days: 210, key: uniqid() },
     { name: 'spinach', days: 55, key: uniqid() },
   ]);
+  const [formVisibility, setFormVisibility] = useState(false);
+  const [inputName, setInputName] = useState('');
+  const [inputDays, setInputDays] = useState('');
+
+  // Toggles form visibility. Sets boolean 'formVisibility' value to the opposite.
+  const toggleFormDisplay = () => {
+    setFormVisibility(!formVisibility);
+  };
+
   return (
     <div className="app">
       <h2>all plants:</h2>
       <List plants={plants} />
+      <div className="inputWrapper">
+        <button className="button showForm" onClick={toggleFormDisplay}>
+          new
+        </button>
+      </div>
+      <Form
+        formVisibility={formVisibility}
+        setFormVisibility={setFormVisibility}
+        inputName={inputName}
+        setInputName={setInputName}
+        inputDays={inputDays}
+        setInputDays={setInputDays}
+        plants={plants}
+        setPlants={setPlants}
+      />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import uniqid from 'uniqid';
+
+const Form = ({
+  formVisibility,
+  setFormVisibility,
+  inputName,
+  setInputName,
+  inputDays,
+  setInputDays,
+  plants,
+  setPlants,
+}) => {
+  // Assigns new class, based on formVisibility.
+  const getVisibility = formVisibility ? 'visible' : 'hidden';
+
+  const handleCloseForm = () => {
+    clearForm();
+    setFormVisibility(false);
+  };
+
+  const handleNameChange = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const handleDaysChange = (event) => {
+    setInputDays(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    // Prevents refresh.
+    event.preventDefault();
+    // Adds to 'plants' array with spread operator.
+    setPlants([...plants, { name: inputName, days: inputDays, key: uniqid() }]);
+    // Deletes all input values from form.
+    clearForm();
+    // Closes the form.
+    handleCloseForm();
+  };
+
+  // Clears all input fields:
+  const clearForm = () => {
+    setInputName('');
+    setInputDays('');
+  };
+
+  return (
+    <div className={`form ${getVisibility}`}>
+      <form className="formWrapper" onSubmit={handleFormSubmit}>
+        <input
+          className="formInput"
+          onChange={handleNameChange}
+          value={inputName}
+          type="text"
+          required
+          placeholder="tomato"
+        />
+        <input
+          className="formInput"
+          onChange={handleDaysChange}
+          value={inputDays}
+          type="number"
+          placeholder="120 days"
+        />
+        <div className="buttons">
+          <button className="button closeForm" onClick={handleCloseForm}>
+            cancel
+          </button>
+          <button className="button submitForm" type="submit">
+            add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
